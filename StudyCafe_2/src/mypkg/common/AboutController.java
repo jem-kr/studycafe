bf86252ae7001a33d6b0369922921a7ebcf9e73e
youngnam <yn4090@gmail.com>
2020-12-23 13:10:25 +0900

    logo, about 수정
    logo클릭하면 main으로 돌아오게, about에 dropdown 메뉴 나오게

--- StudyCafe_2/src/mypkg/common/AboutController.java
+++ StudyCafe_2/src/mypkg/common/AboutController.java
@@ -2,7 +2,6 @@ package mypkg.common;
 
 import java.io.IOException;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,8 +10,8 @@ public class AboutController extends SuperClass{
 
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		super.doGet(request, response);
 		String gotopage ="common/about.jsp";
-		RequestDispatcher dispatcher = request.getRequestDispatcher(gotopage);
-		dispatcher.forward(request, response);
+		super.GotoPage(gotopage);
 	}
 }
