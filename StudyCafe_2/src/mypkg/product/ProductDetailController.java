1b2e80c2d4bde83ab55db762850bc94cffa619b6
youngnam <yn4090@gmail.com>
2021-01-06 18:57:24 +0900

    prDetail
    상세보기 페이지에 p_type에 따른 p_seat 표시

--- StudyCafe_2/src/mypkg/product/ProductDetailController.java
+++ StudyCafe_2/src/mypkg/product/ProductDetailController.java
@@ -1,6 +1,7 @@
 package mypkg.product;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -15,14 +16,21 @@ public class ProductDetailController extends SuperClass {
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String p_seat = request.getParameter("p_seat");
+		
 		ProductDao pdao = new ProductDao();
 		
 		Product bean = pdao.SelectDataByPk(p_seat);
+		
+		String p_type = bean.getP_type();
+		List<Product> glists = pdao.SelectDataByType(p_type);
+
+		
 		
 		String gotopage = "";
 		
 		if (bean != null) {
 			request.setAttribute("bean", bean);
+			request.setAttribute("glists", glists);
 			gotopage = "/product/prDetail.jsp";
 		}else {
 			gotopage = "/product/prList.jsp";
--- StudyCafe_2/src/mypkg/product/ProductListController.java
+++ StudyCafe_2/src/mypkg/product/ProductListController.java
@@ -10,75 +10,25 @@ import javax.servlet.http.HttpServletResponse;
 import mypkg.bean.Product;
 import mypkg.common.SuperClass;
 import mypkg.dao.ProductDao;
-import mypkg.utility.FlowParameters;
-import mypkg.utility.Paging;
 
 public class ProductListController extends SuperClass{
 	
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		FlowParameters parameters 
-			= new FlowParameters(
-					request.getParameter("pageNumber"), 
-					request.getParameter("pageSize"), 
-					request.getParameter("mode"), 
-					request.getParameter("keyword")) ;
-		
-		System.out.println(this.getClass() + " : " + parameters.toString());
 		
 		ProductDao dao = new ProductDao();
 		
-		int totalCount 
-				= dao.SelectTotalCount(
-						parameters.getMode(), 
-						parameters.getKeyword() + "%");
 		
-		String contextPath = request.getContextPath() ;
-		String myurl = contextPath + "/studycafe?command=prList" ;
-		
-		Paging pageInfo = new Paging(
-				parameters.getPageNumber(), 
-				parameters.getPageSize(), 
-				totalCount, 
-				myurl, 
-				parameters.getMode(), 
-				parameters.getKeyword()) ;
-		
-		List<Product> lists = dao.SelectDataList(
-				pageInfo.getBeginRow(), 
-				pageInfo.getEndRow(),
-				parameters.getMode(), 
-				parameters.getKeyword() + "%");
+		List<Product> lists = dao.SelectDataList();
+
 		
 		request.setAttribute("lists", lists);
-		request.setAttribute("pagingHtml", pageInfo.getPagingHtml());
-		request.setAttribute("pagingStatus", pageInfo.getPagingStatus());
-		request.setAttribute("mode", parameters.getMode());
-		request.setAttribute("keyword", parameters.getKeyword());
-		request.setAttribute("parameters", parameters.toString());		
+
 		
 		super.doGet(request, response);
 		String gotopage = "product/prList.jsp" ;
 		super.GotoPage(gotopage);
 	}
 	
-	@Override
-	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		super.doPost(request, response);
-		String gotopage = "" ;
-		if (this.validate(request) == true) {
-			gotopage = "" ;
-			super.GotoPage(gotopage);
-		}else {
-			gotopage = "" ;
-			super.GotoPage(gotopage);
-		}
-	}
-	
-	@Override
-	public boolean validate(HttpServletRequest request) {
-		boolean isCheck = true ;	
-		return isCheck ;
-	}
 	
 }
